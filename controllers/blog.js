50a620c13dece589931776290d602b306a9f31d4
Ryan <ryanwanjiedistortion@gmail.com>
2023-11-19 20:51:30 +0300

    blog-server

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,6 @@
 const blogsModel = require("../models/createblogs");
 const { StatusCodes } = require("http-status-codes");
-
+const mongoose = require("mongoose");
 const CreateBlog = async (req, res) => {
   try {
     const blog = await blogsModel.create({ ...req.body });
@@ -15,4 +15,52 @@ const CreateBlog = async (req, res) => {
   }
 };
 
-module.exports = { CreateBlog };
+const UpdateBlog = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Invalid ID" });
+    }
+
+    const updatedBlog = await blogsModel.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true }
+    );
+
+    if (!updatedBlog) {
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: "Blog not found" });
+    }
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ msg: "Blog was updated successfully", updatedBlog });
+  } catch (err) {
+    //   console.error(err);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
+
+const GetBlogs = async(req, res)=>{
+
+    try{
+
+        const blogs = await blogsModel.find({})
+
+        return res.status(StatusCodes.OK).json({msg:'The blogs are:', blogs})
+
+
+    }
+
+    catch(err){
+        console.log(err)
+        res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+    }
+}
+
+module.exports = { CreateBlog, UpdateBlog,GetBlogs };
--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { CreateBlog } = require("../controllers/blog");
+const { CreateBlog,UpdateBlog,GetBlogs } = require("../controllers/blog");
 
 router.route("/createblog").post(CreateBlog);
+router.route('/updateblog/:id').patch(UpdateBlog)
+router.route('/getblogs').get(GetBlogs)
 
 module.exports = router;
